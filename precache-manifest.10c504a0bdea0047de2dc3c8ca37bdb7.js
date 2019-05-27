1076284f64784a6032473b4a1c9c51e6684ff817
Pisik8809 <pisik8809@gmail.com>
2019-05-27 11:41:56 +0300

    Add files via upload

---
+++ precache-manifest.10c504a0bdea0047de2dc3c8ca37bdb7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ce6b5aa3f8081660e26f54e7ebefde",
+    "url": "/alterrasoft-build/index.html"
+  },
+  {
+    "revision": "3e7de43b8348deb8ef48",
+    "url": "/alterrasoft-build/static/css/2.30d946b3.chunk.css"
+  },
+  {
+    "revision": "97e5fabbe2b7a05b66ce",
+    "url": "/alterrasoft-build/static/css/main.82d60c66.chunk.css"
+  },
+  {
+    "revision": "3e7de43b8348deb8ef48",
+    "url": "/alterrasoft-build/static/js/2.51bd0393.chunk.js"
+  },
+  {
+    "revision": "97e5fabbe2b7a05b66ce",
+    "url": "/alterrasoft-build/static/js/main.727ae96c.chunk.js"
+  },
+  {
+    "revision": "a9377f223f5dd6e6032e",
+    "url": "/alterrasoft-build/static/js/runtime~main.97f1a1f8.js"
+  },
+  {
+    "revision": "81c88265d87a5d0bc5d8b7c79e01b66b",
+    "url": "/alterrasoft-build/static/media/logo.81c88265.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/alterrasoft-build/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/alterrasoft-build/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/alterrasoft-build/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/alterrasoft-build/static/media/slick.f97e3bbf.svg"
+  }
+]);
